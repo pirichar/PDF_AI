9b0cf85309c4eb5dc585df14e0754f842ddc004a
Pier-Luc Richard <84756667+pirichar@users.noreply.github.com>
2025-05-22 16:04:00 -0400

    first commit after creating the hero

---
+++ components/Navbar.tsx
@@ -0,0 +1,117 @@
+"use client"
+
+import Link from "next/link"
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs"
+import { Menu, X } from "lucide-react"
+import { use, useState } from "react"
+
+const NavBar = () =>{
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleMenu = () =>{
+		setIsOpen (!isOpen)
+	}
+
+	const closeMenu	= () =>{
+		setIsOpen(false);
+	}
+
+	return(
+		<nav className="border-b border-purple-300/5 shadow-[0_4px_20px_-10px shadow-purple-200/300">
+			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+				 {/* Desktop Navigation and Mobile Menu Icon*/}
+				<div className="flex items-center justify-between h-20">
+					<Link href="/" className="text-2xl font-bold text-white">
+						PDFtoolAI
+					</Link>
+					<div className="hidden md:flex items-center space-x-4">
+						<Link href="/dashboard" className="text-white/70 hover:text-white px-4 py-2 transition=all duration-300
+														hover:shadow-[0_2px_8px_0] hover:shadow-purple-400/40 hover:rounded-md">
+							Dashboard
+						</Link>
+
+						<Link href="/dashboard" className="text-white/70 hover:text-white px-4 py-2 transition=all duration-300
+														hover:shadow-[0_2px_8px_0] hover:shadow-purple-400/40 hover:rounded-md">
+							Pricing
+						</Link>
+
+						<SignedIn>
+							<SignOutButton>
+								<button className="text-white/70 hover:text-white px-4 py-2 transition=all duration-300
+																hover:shadow-[0_2px_8px_0] hover:shadow-purple-400/40 hover:rounded-md">
+									Signout
+								</button>
+							</SignOutButton>
+						</SignedIn>
+
+						<SignedOut>
+							<div className="flex items-center">
+								<Link href="/sign-in" className="group relative inline-flex items-center gap-2 rounded-full bg-black px-4 py-2 text-white transition-all hover:bg-white/5">
+									<span className="absolute inset-0 rounded-full bg-gradient-to-r from-[#FF1E56] via-[#FF00FF] to-[#00FFFF] opacity-70 blur-sm transition-all group-hover:opacity-100"></span>
+									<span className="aboslute inset-0.5 rounded-full bg-black/50"></span>
+									<span className="relative font-medium">Sign in</span>
+								</Link>
+							</div>
+						</SignedOut>
+					</div>
+
+					{/* Mobile Menu */}
+					<div className="md:hidden z-50">
+						<button
+							onClick={toggleMenu}
+							className="p-2"
+						> 
+							{isOpen ?(
+								<X className="h-6 w-6"/ >
+							): (
+								<Menu className="h-6 w-6"/ >
+							)}
+						</button>
+					</div>
+				</div>
+					
+				{/* Mobile Menu Navigation */}
+				<div className="md:hidden fixed inset-0 z-40">
+					<div className={`absolute inset-0 backdrop-blur-xl transition-opacity duration-300
+						${isOpen ? "opacity-100" : "opacity-0"}`} onClick={closeMenu}/ >
+
+					<div className={`absolute top-16 left-0 right-0 border-b border-purple-300/5 shadow-lg transition-all duration-300 ease-in-out
+						${isOpen ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"}`} onClick={closeMenu}>
+						{/* Main Mobile Navbar Div */}
+						<div className="flex flex-col text-center space-y-4">
+							<Link href="/dashboard" className="text-white/70 hover:text-white px-4 py-2 transition=all duration-300
+															hover:shadow-[0_2px_8px_0] hover:shadow-purple-400/40 hover:rounded-md">
+								Dashboard
+							</Link>
+							<Link href="/dashboard" className="text-white/70 hover:text-white px-4 py-2 transition=all duration-300
+															hover:shadow-[0_2px_8px_0] hover:shadow-purple-400/40 hover:rounded-md">
+								Pricing
+							</Link>
+							<div className="max-w-lg mx-auto mt-2 mb-6">
+								<SignedIn>
+									<SignOutButton>
+										<button className="text-white/70 hover:text-white px-4 py-2 transition=all duration-300
+																		hover:shadow-[0_2px_8px_0] hover:shadow-purple-400/40 hover:rounded-md">
+											Signout
+										</button>
+									</SignOutButton>
+								</SignedIn>
+								<SignedOut>
+										<div className="flex items-center">
+											<Link href="/sign-in" className="group relative inline-flex items-center gap-2 rounded-full bg-black px-4 py-2 text-white transition-all hover:bg-white/5">
+												<span className="absolute inset-0 rounded-full bg-gradient-to-r from-[#FF1E56] via-[#FF00FF] to-[#00FFFF] opacity-70 blur-sm transition-all group-hover:opacity-100"></span>
+												<span className="aboslute inset-0.5 rounded-full bg-black/50"></span>
+												<span className="relative font-medium">Sign in</span>
+											</Link>
+										</div>
+								</SignedOut>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	)
+}
+
+export default NavBar
---
+++ components/landingPage/Hero.tsx
@@ -0,0 +1,81 @@
+"use client"
+
+import { FileText, Search, Zap } from "lucide-react"
+
+const rings = [
+	{width: 300, opacity: 0.8},
+	{width: 500, opacity: 0.7},
+	{width: 700, opacity: 0.6},
+	{width: 900, opacity: 0.5},
+	{width: 1200, opacity: 0.4},
+	{width: 1500, opacity: 0.3},
+	{width: 1700, opacity: 0.2},
+]
+
+const icons = [
+	{
+		icon: FileText,
+		desc: "Analyze any PDF"
+	},
+	{
+		icon: Search,
+		desc: "Etract key Insights"
+	},
+	{
+		icon: Zap,
+		desc: "Save time"
+	}
+]
+
+const Hero = () =>{
+
+
+	return(
+		<div className="min-h-screen relative overflow-hidden flex flex-col items-center justify-center">
+			{/* Rings effect  */}
+			<div className="absolute inset-0 flex justify-center items-center z-0">
+				{rings.map((ring, index) =>(
+					<div key={index} className={`
+						absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+						rounded-full border border-purple-300/20 shadow-[0_0_150px_inset] shadow-purple-200/10
+					`}
+					style={{
+						width:`${ring.width}px`,
+						height:`${ring.width}px`,
+						opacity: ring.opacity
+					}}
+					>
+					</div>
+				))}
+			</div>
+
+		{/* Content */}
+		<div className="z-10 text-center px-4">
+			<h1 className="text-5xl md:text-7xl font-bold mb-6 bg-clip-text text-transparent bg-red-200 bg-gradient-to-b from-white to-purple-200">
+				PDF Analysis tool
+			</h1>
+			<p className="text-lg md:text-xl text-white/70 mb-10 max-w-3xl mx-auto">
+				Extract insights from documents instantly with our AI-Powered analysis tool
+			</p>
+			<a>Glowing Button</a>
+		</div>
+		{/* Feature icons */}
+		<div className="flex flex-wrap justify-center gap-8 mt-16 z-10 px-4">
+			{icons.map((icon, index) =>(
+				<div key={index} className="flex flex-col items-center max-w-[150px]">
+					<div className="w-12 h-12 rounded-full bg-purple-400/10 flex items-center justify-center mb-3">
+						<icon.icon className="text-purple-100 w-6 h-6"/>
+					</div>
+					<p className="text-white/80 text-center text-sm">
+						{icon.desc}
+					</p>
+				</div>
+			))}
+
+		</div>
+
+		</div>
+	)
+}
+
+export default Hero
